3b7d3862ea95dba2216db6946cf34494a1a22355
corollari <github@albert.sh>
2020-12-19 19:47:12 +0100

    Add p4 provided stuff

---
+++ p4/algoritmeLMS.m
@@ -0,0 +1,31 @@
+%
+% =====================
+% === LMS ALGORITHM ===
+% =====================
+%
+% Function that iterates the LMS algorithm.
+%
+% INPUTS
+%%% d:  vector with reference signal.
+%%% x:  vector with input signal.
+%%% P:  number of coefficients of the filter to be estimated (must be even).
+%%% mu: convergence coefficient (non-normalized).
+%
+% OUTPUTS
+%%% H:  Matrix with filter coefficients for each time instant.
+%%%       * Rows      -> coefficients.
+%%%       * Columns   -> time.
+%%% y:  Vector with adaptive filter output as a function of time.
+%%% e:  Vector with error signal as a function of time.
+%
+
+
+function [H, y, e]          =   algoritmeLMS(d, x, P, mu)
+
+
+%% The algorithm must be implemented here.
+
+
+
+
+end
